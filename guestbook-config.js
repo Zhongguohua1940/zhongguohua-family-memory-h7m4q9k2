cc9de3851091c9a638a45e575b96796bda576323
Zhongguohua1940 <lsjys1940@outlook.com>
2026-08-15 16:41:53 +0800

    Connect guestbook to Google Apps Script Web App

--- guestbook-config.js
+++ guestbook-config.js
@@ -1,3 +1,3 @@
 // 观后留言系统配置
-// 第一次启用时，把 Google Apps Script 部署后的“Web 应用”网址粘贴到下面引号里。
-const GUESTBOOK_API_URL = 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE';
+// Google Apps Script Web 应用网址
+const GUESTBOOK_API_URL = 'https://script.google.com/macros/s/AKfycbxTuUudI2Bfg3N5ouI8PQ2g6d2tzMT22inAdOOH4AiW_HmCTirO11SfSdOopdJ4l3RL4A/exec';
